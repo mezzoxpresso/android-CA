b31b608936db4df52d7bfa9b67719feddd94ac34
unknown <kelaniza@gmail.com>
2023-01-04 18:47:41 +0800

    2nd

--- app/src/main/java/iss/workshop/layout/SecondActivity.java
+++ app/src/main/java/iss/workshop/layout/SecondActivity.java
@@ -2,21 +2,16 @@ package iss.workshop.layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         setupBtns();
 //        chooseFirstImage();
     }
